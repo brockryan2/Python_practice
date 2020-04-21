164789a091ab470adfa42b01b2aa5898ad632552
Brock Ryan <brock.ryan2@gmail.com>
2020-04-21 00:28:45 -0400

    Update trying_out_zip.py

--- trying_out_zip.py
+++ trying_out_zip.py
@@ -67,10 +67,10 @@ print(numbers)
 print(letters)
 
 
-#sorting in parallel
+#sorting with zip
 
-letters2 = ['b', 'a', 'd', 'c']
-numbers2 = [2, 4, 1, 3]
+letters1 = ['b', 'a', 'd', 'c']
+numbers1 = [2, 4, 1, 3]
 
 data1 = list(zip(letters1, numbers1))
 data2 = list(zip(numbers1, letters1))
@@ -79,3 +79,44 @@ data1.sort()
 data2.sort()
 
 print("sorted by letter:", data1)
+print("sorted by number:", data2)
+
+# parallel sorting with zip
+
+letters2 = ['b', 'a', 'd', 'c']
+numbers2 = [2, 4, 3, 1]
+
+data3 = sorted(zip(letters, numbers))
+print(data3)
+
+
+# calculating in pairs
+
+total_sales = [52000.00, 51000.00, 48000.00]
+prod_costs  = [46800.00, 45900.00, 43200.00]
+total_profit = 0
+
+for sales, costs in zip(total_sales, prod_costs):
+  profit = sales - costs
+  total_profit += profit
+  print('profit =', profit)
+
+print("total profit = ", total_profit)
+
+
+# building dictionaries
+
+fields = ['name', 'age', 'fav_color']
+values = ['Bob', 2, 'blue']
+
+a_dict = dict(zip(fields, values))
+print(a_dict)
+
+
+def custom_zip(iterable1, *args):
+
+  if hasattr(iterable1, '__iter__'):
+    for arg in args:
+      if hasattr(arg, '__iter__'):
+        for i in arg:
+
