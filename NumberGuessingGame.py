bca19112c8edaf6fc5e020e89d397a0a3af3af2f
Brock <brock.ryan2@gmail.com>
2020-07-26 17:01:43 -0400

    Update NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,11 +1,23 @@
 from random import randint
+import sys
 
-answer = randint(0, 10)
+answer = randint(0, 100)
+
+guess = None
 
 try:
-	guess = int(input("The computer has chosen an integer between 0 and 10. Can you guess what it is?\nEnter your guess >>> "))
+	guess = int(input("The computer has chosen an integer between 0 and 100. Can you guess what it is?\nEnter your guess >>> "))
 
-except typeError as te:
+except ValueError as te:
+	print("Invalid input! Please only enter integer values. Try again.")
+
+	try:
+		guess = int(input("\n \nEnter your guess >>> "))
+
+	except ValueError as te:
+		print("Invalid input! Please try again later.")
+		sys.exit(1)
+
 	
 
 while(guess != answer):
@@ -14,6 +26,10 @@ while(guess != answer):
 
 	elif(guess < answer):
 		print("Your guess is too low. Try again.\n >>> ")
+
+	else:
+		print("An error has ocurred. Please try to run the program again.")
+		sys.exit(1)
 
 	guess = int(input())
 
